ebc1c22dc28c880a6ccfc004b50453245e638a11
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-05-16 23:58:43 +0530

    Setting up basic outlet

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Layout from './Layout';
+import About from './About/About'
+import JobRoles from './JobRoles/JobRoles';
+
+export { Header, Footer, Layout, About, JobRoles };
